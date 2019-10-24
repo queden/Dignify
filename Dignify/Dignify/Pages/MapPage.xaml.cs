3a8239cf0bbcdf036ec32a4f9e9f5ef60a29efdc
queden <cadenmfelton@gmail.com>
2019-10-24 17:41:04 -0500

    Added Xamarin.Forms.Maps integration

--- Dignify/Dignify/Pages/MapPage.xaml.cs
+++ Dignify/Dignify/Pages/MapPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
 
 namespace Dignify.Pages
 {
@@ -14,8 +15,18 @@ namespace Dignify.Pages
     {
         public MapPage()
         {
-            InitializeComponent();
+            var map = new Map(
+                MapSpan.FromCenterAndRadius(
+                        new Position(37, -122), Distance.FromMiles(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
         }
-        
     }
 }
